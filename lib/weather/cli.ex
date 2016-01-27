1e1773817d4b3b7ba6d49fcd3794c5a689f0a3ab
greggreg <greggreg@itsgreggreg.com>
2016-01-27 09:20:06 -0600

    added celcius and better switch parsing

--- lib/weather/cli.ex
+++ lib/weather/cli.ex
@@ -1,8 +1,8 @@
 defmodule Weather.CLI do
   import Weather.LocationParser,  only: [parse_location: 1]
 
-  @switches [short: :boolean]
-  @switch_aliases [s: :short]
+  @switches [short: :boolean, celcius: :boolean]
+  @switch_aliases [s: :short, c: :celcius]
 
   def main(argv) do
     weather_data = ProgressBar.render_spinner([frames: :braille, done: :remove], fn ->
@@ -19,11 +19,25 @@ defmodule Weather.CLI do
 
   def parse_args(argv) do
     args = OptionParser.parse(argv, switches: @switches, aliases: @switch_aliases)
+    |> parse_unknown_args
     case args do
       {switches,[],[]} -> {:no_location, switches}
       {switches,location,[]} ->
         {Enum.join(location, " ") |> parse_location, switches}
     end
+  end
+
+  def parse_unknown_args({a,b,[]}), do: {a,b,[]}
+  def parse_unknown_args({switches,b,[{switch,_}|_]}) do
+    letters = tl(String.split(switch, ""))
+    switches = List.foldl(letters, switches, fn(letter, acc) ->
+      if alias = Keyword.get(@switch_aliases, String.to_atom(letter)) do
+        [{alias, true}|acc]
+      else
+        acc
+      end
+    end)
+    {switches, b, []}
   end
 
   def process({:no_location, switches}) do
--- lib/weather/formatter.ex
+++ lib/weather/formatter.ex
@@ -3,7 +3,7 @@ defmodule Weather.Formatter do
   def puts(weather, location, switches) do
     IO.puts("""
       Current Weather in #{location.city} \
-      #{round(weather["currently"]["temperature"])}°F and \
+      #{temp(weather["currently"]["temperature"], switches, true)} and \
       #{String.capitalize weather["currently"]["summary"]}\
       """)
 
@@ -16,10 +16,12 @@ defmodule Weather.Formatter do
       |> space_and_join(3, " ")
       IO.puts "  Time: #{times}"
 
+      unit = if {:celcius, true} in switches, do: "C", else: "F"
       temps = pluck(hourly_data, "temperature")
       |> Enum.map(&round(&1))
+      |> Enum.map(&temp(&1, switches))
       |> space_and_join(3, " ")
-      IO.puts "  Temp: #{temps}°F"
+      IO.puts "  Temp: #{temps}°#{unit}"
 
       precips = pluck(hourly_data, "precipProbability")
       |> Enum.map(&(round(&1*100)))
@@ -48,6 +50,22 @@ defmodule Weather.Formatter do
   defp space_and_join(list, spaces, sep) do
     Enum.map(list, &String.ljust("#{&1}", spaces))
     |> Enum.join(sep)
+  end
+
+  defp temp(num, switches, false), do: temp(num, switches)
+  defp temp(num, switches, true) do
+    unit = (case {:celcius, true} in switches do
+      true  -> "°C"
+      false -> "°F"
+    end)
+    "#{temp(num, switches)}#{unit}"
+  end
+
+  defp temp(num, switches) do
+    if {:celcius, true} in switches do
+      num = (num - 32) / 9 * 5
+    end
+    round(num)
   end
 
 end
